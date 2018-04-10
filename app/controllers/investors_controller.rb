547380fe94548afae93d8f804afc7e0866c73662
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-04-10 17:51:08 -0300

    investor controller started

---
+++ app/controllers/investors_controller.rb
@@ -0,0 +1,29 @@
+class InvestorsController < ApplicationController
+
+  def new
+    @investor = investor.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+
+  def investor_params
+    params.require(:user).permit(:first_name, :last_name, :monthly_income, :cpf, :date_of_birth,
+      :gender, :mother_name, :father_name, :phone, :address, :net_worth)
+  end
+

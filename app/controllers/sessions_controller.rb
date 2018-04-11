2328cd9a4cbc19289d9f01fc9f943c7e57f13096
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-04-11 16:55:25 -0300

    sign-up $ sign-in paths done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_company == false
+      deals_path
+    elsif resource.is_company == true
+      companies_path
+    end
+  end
+
+end

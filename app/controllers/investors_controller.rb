9c7bc01a93cc0ace06dc0bc1b004dc0d9d7f21f9
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-04-11 09:08:34 -0300

    new and create investor

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,18 +1,26 @@
 class InvestorsController < ApplicationController
 
+  before_action :set_investor, only: [:show, :edit, :destroy]
 
   def new
-    @investor = investor.new
+    @investor = Investor.new
   end
 
   def create
-
+    @investor = Investor.new(investor_params)
+    @investor.user = current_user
+     if @investor.save
+      redirect_to investors_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    @investor = Investor.update(investor_params)
   end
 
   def index
@@ -23,8 +31,12 @@ class InvestorsController < ApplicationController
 
   private
 
+  def set_investor
+    @investor = Investor.find(params[:id])
+  end
+
   def investor_params
-    params.require(:user).permit(:first_name, :last_name, :monthly_income, :cpf, :date_of_birth,
+    params.require(:investor).permit(:first_name, :last_name, :monthly_income, :cpf, :date_of_birth,
       :gender, :mother_name, :father_name, :phone, :address, :net_worth)
   end
 

a2fc98c1c4d108ea224d597d4d7fe3eaec1d34f1
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-04-13 12:21:46 -0300

    seedsupdate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411171545) do
+ActiveRecord::Schema.define(version: 20180413144736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180411171545) do
     t.string "use_of_proceeds"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 puts "Please wait while the seeds load..."
 
 puts "-"
 
 
-  20.times do |x|
+  10.times do |x|
     user = User.create!(email: Faker::Internet.email, password: "123456")
-    if x.odd?
-      Company.create!(user_id: user.id,
-                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
-                     cnpj: Faker::Number.number(14),
-                     legal_name: Faker::Company.name,
-                     address: Faker::Address.street_address,
-                     phone: Faker::PhoneNumber.cell_phone,
-                     manager_first_name: Faker::Name.first_name,
-                     manager_last_name: Faker::Name.last_name,
-                     manager_cpf: Faker::Number.number(11),
-                     manager_phone: Faker::PhoneNumber.cell_phone,
-                     manager_email: Faker::Internet.email,
-                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
-                     current_billing: Faker::Number.between(100000, 1000000),
-                     website: "www." + Faker::Internet.domain_name,
-                     number_of_employees: Faker::Number.between(1, 40),
-                     use_of_proceeds: Faker::Company.bs
-                     )
-        else x.even?
       Investor.create!(user_id: user.id,
                       first_name: Faker::Name.first_name,
                       last_name: Faker::Name.last_name,
@@ -45,15 +28,136 @@ puts "-"
                       address:Faker::Address.street_address,
                       net_worth:Faker::Number.between(1000000, 10000000)
                       )
-    end
   end
 
 
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 11,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'Le Wagon',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631739/le-wagon-logo.png'
+                     )
 
-9.times do |x|
-  Deal.create(company_id: x + 1,
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 12,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'Lev',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631650/mcwlzwfuy3jygdjqdl4q.jpg'
+                     )
+
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 13,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'Longarina',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631676/iSUBSRk.png'
+                     )
+
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 14,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'Brownie do Luiz',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631697/brownie-do-luiz-logo-300x300.png'
+                     )
+
+
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 15,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'Polis Sucos',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631624/18935-polis-sucos.jpg'
+                     )
+
+user = User.create!(email: Faker::Internet.email, password: "123456")
+      Company.create!(user_id: 16,
+                     date_of_foundation: Faker::Date.between(9.years.ago, 1.year.ago),
+                     cnpj: Faker::Number.number(14),
+                     legal_name: 'City Model Ipanema',
+                     address: Faker::Address.street_address,
+                     phone: Faker::PhoneNumber.cell_phone,
+                     manager_first_name: Faker::Name.first_name,
+                     manager_last_name: Faker::Name.last_name,
+                     manager_cpf: Faker::Number.number(11),
+                     manager_phone: Faker::PhoneNumber.cell_phone,
+                     manager_email: Faker::Internet.email,
+                     company_description: Faker::Company.catch_phrase + ": " +Faker::TwinPeaks.quote,
+                     current_billing: Faker::Number.between(100000, 1000000),
+                     website: "www." + Faker::Internet.domain_name,
+                     number_of_employees: Faker::Number.between(1, 40),
+                     use_of_proceeds: Faker::Company.bs,
+                     photo: 'http://res.cloudinary.com/dta0lx6co/image/upload/v1523631580/XvjOuM9t_400x400.jpg'
+                     )
+
+
+
+6.times do |x|
+  Deal.create!(company_id: x + 1,
               start_date: Faker::Date.between(100.days.ago, 21.days.ago),
-              end_date: Faker::Date.between(20.days.ago, Date.today),
+              end_date: Faker::Date.between(200.days.from_now, 500.days.from_now),
               amount: Faker::Number.between(25000, 1000000),
               rate_per_annum: Faker::Number.between(15, 25).to_s + "," + "#{(0..100).to_a.sample}",
               credit_rating: ['AAA', 'AA', 'A', 'BBB', 'BB', 'B', 'CCC', 'CC', 'C', 'D'].sample,
@@ -64,8 +168,8 @@ end
 
 20.times do |x|
 
-  Transaction.create(investor_id: (1..10).to_a.sample,
-                     deal_id: (1..9).to_a.sample,
+  Transaction.create!(investor_id: (1..10).to_a.sample,
+                     deal_id: (1..6).to_a.sample,
                      participation: [2000, 4000, 8000].sample,
                      )
 end

710f39dbe5082c008dc2c4f5ac0346462b9ace7d
Gianluca Perrone <perronegian@hotmail.com>
2018-04-16 17:24:54 -0300

    geocoding done ready for production

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -17,6 +17,9 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
+  lookup:    :google,
+  api_key:   ENV['GOOGLE_API_SERVER_KEY'],
+  use_https: true,
   units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )

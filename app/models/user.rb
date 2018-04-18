722fa47329e7173d82a884ab4cad1bfc6e1064d9
Gianluca Perrone <perronegian@hotmail.com>
2018-04-18 17:07:22 -0300

    mailer now back to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_one :investor
   has_one :company
-  # after_create :send_welcome_email
+  after_create :send_welcome_email
 
   private
 
-  # def send_welcome_email
-  #   UserMailer.welcome(self).deliver_now
-  # end
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 
 end

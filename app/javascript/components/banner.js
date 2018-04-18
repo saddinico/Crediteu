8cf279c9dda34717ae54f89b9f0be9079abccc72
andre rache <andrezra@gmail.com>
2018-04-18 14:08:00 -0300

    new transaction is now a nice popup

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,10 +2,8 @@ import swal from 'sweetalert';
 
 function bindSweetAlertButtonDemo() {
   document.getElementById('sweet-alert-demo').addEventListener('click', () => {
-    swal({
-      title: "A nice alert",
-      text: "This is a great alert, isn't it?",
-      icon: "success"
+    swal("Quanto você deseja investir?", {
+      content: "input",
     })
   });
 }

39b0b898449d49c908464609080d6e142e56ccf1
Gianluca Perrone <perronegian@hotmail.com>
2018-04-18 18:08:52 -0300

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@ class User < ApplicationRecord
   has_one :company
   after_create :send_welcome_email
 
+
   private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
   end
+
+
 
 end

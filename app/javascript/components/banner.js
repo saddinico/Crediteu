32ceb67fac2ccb797caf7230799ccb6c0062bbdb
andre rache <andrezra@gmail.com>
2018-04-19 14:11:11 -0300

    sd

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,9 +1,12 @@
 import swal from 'sweetalert';
 
 function bindSweetAlertButtonDemo() {
+
   document.getElementById('sweet-alert-demo').addEventListener('click', () => {
-    swal("Quanto você deseja investir?", {
-      content: "input",
+    swal({
+      title: "Obrigado!",
+      text: "Obrigado por se inscrever na nossa lista!",
+      icon: "success"
     })
   });
 }
